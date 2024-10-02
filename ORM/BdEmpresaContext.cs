3e11c99d14dae4b749d69725fb8d0430fd00447c
Hokague <criativasayte@gmail.com>
2024-10-02 11:45:58 -0300

    ver 01

---
+++ ORM/BdEmpresaContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoWebAPI.ORM;
+
+public partial class BdEmpresaContext : DbContext
+{
+    public BdEmpresaContext()
+    {
+    }
+
+    public BdEmpresaContext(DbContextOptions<BdEmpresaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TbFuncionario> TbFuncionarios { get; set; }
+
+    public virtual DbSet<TbUsuario> TbUsuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=LAB10_29\\MSSQLSERVER01;Database=BD_EMPRESA;User Id=admin;Password=12345678;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TbFuncionario>(entity =>
+        {
+            entity.ToTable("TB_FUNCIONARIO");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Foto).HasColumnName("foto");
+            entity.Property(e => e.Idade).HasColumnName("idade");
+            entity.Property(e => e.Nome)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nome");
+        });
+
+        modelBuilder.Entity<TbUsuario>(entity =>
+        {
+            entity.ToTable("TB_USUARIO");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Senha)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("senha");
+            entity.Property(e => e.Usuario)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("usuario");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
